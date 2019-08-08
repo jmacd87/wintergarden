5e81ec5644e2bbf49b11e128624240ea919bbed3
joel <jmacd87@gmail.com>
2019-08-08 18:32:21 -0300

    scrolling

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -8,31 +8,45 @@ export default class ProductList extends Component {
     return (
       
       <React.Fragment>  
-        <body>  
+        <div className="parralax">  
               <section class="section section-dark">
             <div className="valley">
               <div className="pimg1">
-                <span className="border">
             <img src="https://i.pinimg.com/originals/27/a6/68/27a66840d494016ff0b634c607610105.jpg" className="pimg1" alt="valley"/>
-            <div class="centered">Limitless Valley</div>
-            </span>
-            </div></div>
+            <div className="centered">Limitless Valley</div>
+            </div>
+            </div>
             </section>
-            <div class="container">
-            <div className="fox">
-              <img src="https://i.pinimg.com/236x/fc/ed/53/fced53f591c806c660a6306567f0b528--art-fox-watercolor-animals.jpg" className="pimg1" alt="fox"/>
-            </div>
-            
-           
-            <div className="moose">
-              <img src="https://render.fineartamerica.com/images/rendered/medium/poster/images/artworkimages/medium/1/moose-nastasia-cook.jpg" className="moose" alt="moose"/>
-              <img src={sticker} className="sticker"/>
-            </div>
-            <div className="bluejay">
-            <img src="https://dailypainters.com/artists/Paul-Wolber/paintings/full/2012/861/blue_jay___soft_landing.jpg" className="bluejay" alt="bluejay"/>
+
+            <section class="section section-dark">
+            <div className="forest">
+              <div className="pimg2">
+            <img src={require('../Media/moose.png')} className="moose"/>
+              <p>lorem asl;kasjd;flkajsdl;flkajsdl </p>
             </div>
             </div>
-</body>
+            </section>
+
+            <section class="section section-dark">
+            <div className="trees">
+              <div className="pimg3">
+            <img src={require('../Media/hummingbird.png')} className="moose"/>
+              <p>lorem asl;kasjd;flkajsdl;flkajsdl </p>
+            </div>
+            </div>
+            </section>
+
+            <section class="section section-dark">
+            <div className="hills">
+              <div className="pimg4">
+            <img src={require('../Media/fox.png')} className="moose"/>
+              <p>lorem asl;kasjd;flkajsdl;flkajsdl </p>
+            </div>
+            </div>
+            </section>
+
+
+</div>
       </React.Fragment>
       // <Product />
     );
